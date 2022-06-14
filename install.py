85fc3630abbec080656cd693f0a3d64243986692
silverace_71 <alekshilderbrand@gmail.com>
2022-06-13 23:26:50 -0600

    balls

--- install.py
+++ install.py
@@ -1,8 +1,11 @@
+import imp
 import subprocess
+import os
+user=os.environ["USER"]
 subprocess.run(["bash","-c","sudo curl -fsSL https://get.docker.com -o get-docker.sh"])
 subprocess.run(["bash","-c","sudo sh get-docker.sh"])
-subprocess.run(["bash","-c","sudo usermod -aG docker ${USER}"])
+subprocess.run(["bash","-c","sudo usermod -aG docker "+user])
 subprocess.run(["bash","-c","sudo docker version"])
 subprocess.run(["bash","-c","sudo apt-get install libffi-dev libssl-dev && sudo apt install python3-dev && sudo apt-get install -y python3 python3-pip -y"])
 subprocess.run(["bash","-c","sudo pip3 install docker-compose"])
-print("PLEASE REBOOT OR RELOG")
+print("\u001b[36mPLEASE REBOOT OR RELOG\u001b[0m")
