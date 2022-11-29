b71e17998667b0a434e015c26e399ec3463c5bdf
silverace_71 <alekshilderbrand@gmail.com>
2022-11-28 19:06:34 -0700

    fixed docker port issue

--- install-all-docker.sh
+++ install-all-docker.sh
@@ -17,11 +17,11 @@ read confirm
             sudo apt-get install -y python3 python3-pip
             sudo pip3 install docker-compose
             sudo docker volume create portainer_data
-            sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+            sudo docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 ##telling you where portainer is
     lanIp="$(ip -4 -o -br addr|awk '$0 ~ /^[we]\w+\s+UP\s+/ {str = gsub("/[1-9][0-9]*", "", $0); print $3}')";
 
-        echo "Portainer is located at: https://${lanIp}:9000";
+        echo "Portainer is located at: https://${lanIp}:9443";
         sleep 3
 ##reboot time
     echo "it is HIGHLY reccomended that you reboot your system, do you wish to reboot now (y/n)"
