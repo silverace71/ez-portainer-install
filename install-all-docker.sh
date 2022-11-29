a3029a883b0ffa2f4334838b2bf4c6d6e2cb3e68
silverace_71 <alekshilderbrand@gmail.com>
2022-11-28 17:48:19 -0700

    changing python to bash

---
+++ install-all-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "docker, docker compose, and portainer, alongside any other dependencies [YOU HAVE TO REBOOT YOUR PC]"
+
+sleep 3
+
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
