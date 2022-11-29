9c4e85d396f280efac862e5dfdf5b5f4ce1cc7c0
silverace_71 <alekshilderbrand@gmail.com>
2022-11-28 19:08:19 -0700

    fixed portainer port issue

--- install-all-docker.sh
+++ install-all-docker.sh
@@ -17,6 +17,7 @@ read confirm
             sudo apt-get install -y python3 python3-pip
             sudo pip3 install docker-compose
             sudo docker volume create portainer_data
+            sudo docker pull portainer/portainer-ce:latest
             sudo docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 ##telling you where portainer is
     lanIp="$(ip -4 -o -br addr|awk '$0 ~ /^[we]\w+\s+UP\s+/ {str = gsub("/[1-9][0-9]*", "", $0); print $3}')";
