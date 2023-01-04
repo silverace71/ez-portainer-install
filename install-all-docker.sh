c56bab45e0f796373f088a052b4cc89acd34b107
silverace_71 <alekshilderbrand@gmail.com>
2023-01-03 19:21:12 -0700

    made this work for more distros

--- install-all-docker.sh
+++ install-all-docker.sh
@@ -4,18 +4,43 @@
 echo "docker, docker compose, and portainer, alongside any other dependencies, do you wish to continue (y/n)"
 read confirm
     if [[ $confirm == "y" ]]; then
-        echo "please type your username"
-            read name
+        name=$(whoami)
         echo "Installing Docker"
 ##This is where all the commands are
-            sudo apt update
+
+            sudo usermod -aG docker $name
+
+            if [ -x "$(command -v apt-get)" ]; then
             sudo curl -fsSL https://get.docker.com -o get-docker.sh
             sudo sh get-docker.sh
-            sudo usermod -aG docker $name
             sudo apt-get install libffi-dev libssl-dev
             sudo apt install python3-dev -y
             sudo apt-get install -y python3 python3-pip
             sudo pip3 install docker-compose
+            sudo docker volume create portainer_data
+            sudo docker pull portainer/portainer-ce:latest
+            sudo docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+            elif [ -x "$(command -v dnf)" ]; then
+            sudo dnf -y install dnf-plugins-core
+            sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+            sudo dnf install docker-ce docker-ce-cli containerd.io
+            sudo docker volume create portainer_data
+            sudo docker pull portainer/portainer-ce:latest
+            sudo docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+            elif [ -x "$(command -v yum)" ]; then
+            sudo yum -y install yum-utils
+            sudo yum-config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+            sudo yum install docker-ce docker-ce-cli containerd.io
+            sudo docker volume create portainer_data
+            sudo docker pull portainer/portainer-ce:latest
+            sudo docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+            elif [ -x "$(command -v pacman)" ]; then
+            sudo pacman -S docker
+            sudo systemctl start docker
+            sudo systemctl enable docker
+            sudo pacman -S docker-compose
+            sudo systemctl start docker-compose
+            sudo systemctl enable docker-compose
             sudo docker volume create portainer_data
             sudo docker pull portainer/portainer-ce:latest
             sudo docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
